d26238c7d5ffb39770441d8edc88aff6a2b3fa6d
caua h <cauahmourarodrigues@gmail.com>
2025-09-09 15:05:50 -0300

    inicializando a classe paciente

---
+++ classes/Paciente.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Paciente {
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Paciente(String nome, String cpf, int idade){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.idade = idade;
+    }
+}
+
